a03591e0937cc914aaf1a5a692693084f0a44514
andref-hub <114247626+andref-hub@users.noreply.github.com>
2022-12-19 08:40:39 -0300

    Loopings

---
+++ Desenvolvimento Web/Tópico 3 - JavaScript/loops/for.js
@@ -0,0 +1,7 @@
+for(let i=0; i<10; i++){
+    console.log(i);
+}
+
+for(let a=10; a>0; a--) {
+    console.log(a);
+}
---
+++ Desenvolvimento Web/Tópico 3 - JavaScript/loops/while.js
@@ -0,0 +1,13 @@
+let quantidadeA = 10;
+let contadorA = 0;
+
+while (contadorA < quantidadeA) {
+    console.log(++contadorA);
+}
+
+let quantidadeB = 0;
+let contadorB = 10;
+
+while (contadorB > quantidadeB) {
+    console.log(contadorB--);
+}
